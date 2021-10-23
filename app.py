414a7cb912e802ed18c56412768f32e7003a8de7
Vitalii <vitalikkalyta96@gmail.com>
2021-10-23 13:17:41 +0300

    To added files.

--- app.py
+++ app.py
@@ -1,47 +1,14 @@
-from flask import Flask, render_template
+from flask import Flask
+from config import Config
 
 
 app = Flask(__name__)
 
-@app.route('/', methods=["GET"])
-def index():
-    return render_template('index.html')
-    
-def calculator(val_one, val_two, action):
-    try:
-        val_one = int(val_one)
-    except ValueError:
-        return 'val_one is bad'
-    try:
-        val_two = int(val_two)
-    except ValueError:
-        return 'val_two is bad'
-    if action == '/':
-        result = val_one / val_two
-    elif action == '+':
-        result = val_one + val_two
-    elif action == '-':
-        result = val_one - val_two
-    elif action == '*':
-        result = val_one * val_two
-    else:
-    	result = 'nothing'
-    return result
-    
-@app.route('/calc/<int:value_one>/<int:value_two>/<oper>', methods=["GET", "POST"])
-def act(value_one, value_two, oper):
-    actions = ['/', '+', '-', '*']
-    if oper == 'div':
-        oper = '/'
-    elif oper == 'sum':
-        oper = '+'
-    elif oper == 'dif':
-        oper = '-'
-    elif oper == 'mul':
-        oper = '*'
-    result = calculator(value_one, value_two, oper)
-    return render_template('result.html', num_one=value_one, num_two=value_two, action=oper, result=result)
+print(Config.WEATHER_API_KEY)
+print(Config.WEATHER_API_URL)
 
+with app.app_context():
+    from routes.lecture1 import *
+    from routes.weather import *
 
-if __name__=="__main__":
-    app.run(host='0.0.0.0', port=9090, debug=True)
+app.run(debug=True)
