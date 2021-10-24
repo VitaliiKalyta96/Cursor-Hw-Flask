64936d0f5d90ff6488f550e369e7c62cc88ceab4
Vitalii <vitalikkalyta96@gmail.com>
2021-10-24 16:02:55 +0300

    Updated files.

--- tests_2/unittest_weather.py
+++ tests_2/unittest_weather.py
@@ -7,5 +7,5 @@ class TestFoo(flast_unittest.ClientTestCase):
 
     def test_weather(self, client):
         response = client.get('/weather?city=Kiev')
-        self.assertInResponse(response.json['location']['name'], "Kiev")
+        self.assertJsonEqual(response.json['location']['name'], "Kiev")
         self.assertStatus(response.response_code, 200)
