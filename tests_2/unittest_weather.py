3129dc0842b2e54fed0097c4ac25c7e2bc5d82c2
Vitalii <vitalikkalyta96@gmail.com>
2021-10-24 15:51:53 +0300

    Updated files.

---
+++ tests_2/unittest_weather.py
@@ -0,0 +1,11 @@
+import flask_unittest
+from app import app
+from .conftest import client
+
+
+class TestFoo(flast_unittest.ClientTestCase):
+
+    def test_weather(self, client):
+        response = client.get('/weather?city=Kiev')
+        self.assertInResponse(response.json['location']['name'], "Kiev")
+        self.assertStatus(response.response_code, 200)
