1cb969519a7d8fc3fed690617623d5115c9352a6
Vitalii <vitalikkalyta96@gmail.com>
2021-10-30 21:45:25 +0300

    Yet resolving 15 tasks.

--- flask3/routes/api/todo.py
+++ flask3/routes/api/todo.py
@@ -4,6 +4,15 @@ from flask_restful import Resource
 
 todos = []
 
+# Створив функцію для діставання title, text для редагування і видалення даних.
+# Чи варто її створювати чи є якийсь інший шлях для доступу до даних ???
+
+def get_user_by_data(user_data):
+    for x in todos:
+        if x.get("title", "text") == str(user_data):
+            return x
+
+
 class Todo(Resource):
     def get(self):
         return todos
@@ -12,4 +21,31 @@ class Todo(Resource):
         todos.append(request.json)
         return todos
 
+# Створив окермий клас для вирішення 1 завадння 15 дз. Чи я на вірному шляху ?
+# Гуглив однак там в методи класу додають переважно ід чи тут потрібно також ід чи потрібно через title, text??
+#
+
+class Todo2(Resource):
+
+    def get(self, title, text):
+        user = get_user_by_data(title, text)
+        if not user:
+            return {"error": "User not found"}
+        return user
+        
+    def put(self, title, text):
+        user = get_user_by_data(title, text)
+        if user:
+            todos.remove(user)
+            todos.append(request.json)
+        return todos        
+
+    def delete(self, title, text):
+        return delete_todo()
+        user = get_user_by_data(title, text)
+        if user:
+            todos.remove(user)
+        return {"message": "Deleted"}
+        
 api.add_resource(Todo, "/api/v1/todos")
+api.add_resource(Todo2, "/api/v1/todos/<str:title>/<str:text>", methods=['PUT', 'DELETE'])
--- flask3/routes/todo.py
+++ flask3/routes/todo.py
@@ -1,12 +1,23 @@
 from app import app
-from flask import render_template
+from flask import render_template, jsonify
 
 
 @app.route('/todos')
 def todos_list():
     return render_template('todo.html')
 
-
 @app.route('/add-todo')
 def add_todo():
     return render_template('add-todo.html')
+    
+    
+# Чи варто зачіпати цю todo.py цей блок для вирішення 1 завдання з додаванням методу апдейт і деліт ??
+
+# @app.route('/update-todo/<string:title>/<string:text>/', methods=['PUT'])
+# def update_todo(title, text):
+#     return jsonify({title=title, text=text})
+
+# @app.route('/delete-todo/<string:title>/<string:text>/', methods=['DELETE'])
+# def delete_todo(title, text):
+#    todos.remove(title[0], text[1])
+#    return None, 404
