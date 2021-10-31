7327335bde0dc5244f451212c90bd0c2ac152188
Vitalii <vitalikkalyta96@gmail.com>
2021-10-31 13:17:48 +0200

    Updated files.

--- flask3/routes/api/todo.py
+++ flask3/routes/api/todo.py
@@ -4,14 +4,6 @@ from flask_restful import Resource
 
 todos = []
 
-# Створив функцію для діставання title, text для редагування і видалення даних.
-# Чи варто її створювати чи є якийсь інший шлях для доступу до даних ???
-
-def get_user_by_data(user_data):
-    for x in todos:
-        if x.get("title", "text") == str(user_data):
-            return x
-
 
 class Todo(Resource):
     def get(self):
@@ -22,30 +14,39 @@ class Todo(Resource):
         return todos
 
 # Створив окермий клас для вирішення 1 завадння 15 дз. Чи я на вірному шляху ?
-# Гуглив однак там в методи класу додають переважно ід чи тут потрібно також ід чи потрібно через title, text??
+# Гуглив однак там в методи класу додають переважно ід чи тут потрібно також ід чи потрібно через title??
 #
 
 class Todo2(Resource):
 
-    def get(self, title, text):
-        user = get_user_by_data(title, text)
+    def get(self, title):
+        user = get_user_by_data(title)
         if not user:
             return {"error": "User not found"}
         return user
         
-    def put(self, title, text):
-        user = get_user_by_data(title, text)
+    def put(self, title):
+        user = get_user_by_data(title)
         if user:
             todos.remove(user)
             todos.append(request.json)
         return todos        
 
-    def delete(self, title, text):
+    def delete(self, title):
         return delete_todo()
-        user = get_user_by_data(title, text)
+        user = get_user_by_data(title)
         if user:
             todos.remove(user)
         return {"message": "Deleted"}
+
+
+# Створив функцію для діставання title для редагування і видалення даних.
+# Чи варто її створювати чи є якийсь інший шлях для доступу до даних ???
+
+def get_user_by_data(user_data):
+    for x in todos:
+        if x.get("title") == int(user_data):
+            return x
         
 api.add_resource(Todo, "/api/v1/todos")
-api.add_resource(Todo2, "/api/v1/todos/<str:title>/<str:text>", methods=['PUT', 'DELETE'])
+api.add_resource(Todo2, "/api/v1/todos/<string:title>", methods=['PUT', 'DELETE'])
