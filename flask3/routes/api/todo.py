3f8ca8ec73b7a705ee3f2e8fb45a6e4fd63dff18
Vitalii <vitalikkalyta96@gmail.com>
2021-11-02 00:08:57 +0200

    Updated api todo.py.

--- flask3/routes/api/todo.py
+++ flask3/routes/api/todo.py
@@ -1,9 +1,9 @@
 from app import app, api
-from flask import request
+from flask import request, Response
 from flask_restful import Resource
 
-todos = []
 
+todos = []
 
 class Todo(Resource):
     def get(self):
@@ -14,33 +14,41 @@ class Todo(Resource):
         return todos
         
         
-def get_by_data(data):
-    for x in todos:
-        if x.get("title") == str(data):
-            return x
+# def get_by_data(data):
+#    for x in todos:
+#        if x.get("title") == int(data):
+#            return x
 
 
 class Todo2(Resource):
 
+#    def get(self, title):
+#       user = get_by_data(title)
+#        if not user:
+#            return {"error": "User not found"}
+#        return user
+
     def get(self, title):
-        user = get_by_data(title)
-        if not user:
-            return {"error": "User not found"}
-        return user
+        data = todos[title]
+        if not data in todos:
+            return Response("Data not found", 404)
+        return todos[title]
         
-    def put(self, title):
-        user = get_by_data(title)
-        if user:
-            todos.remove(user)
-            todos.append(request.json)
-        return todos        
+#    def put(self, title):
+#        user = get_by_data(title)
+#        if user:
+#            todos.remove(user)
+#            todos.append(request.json)
+#        return todos  
+
+    def put(self, title):  
+        todos[title] =  request.json.get('text')
+        return todos[title]
 
     def delete(self, title):
-        return delete_todo()
-        user = get_by_data(title)
-        if user:
-            todos.remove(user)
-        return {"message": "Deleted"}
+        del todos[title]
+        return Response("", 204)
         
 api.add_resource(Todo, "/api/v1/todos")
-api.add_resource(Todo2, "/api/v1/todos/<string:title>", methods=['PUT', 'DELETE'])
+api.add_resource(Todo2, "/api/v1/todos/<int:title>", methods=['PUT', 'DELETE'])
+
