bd7d8f186464b4ba769b1edfbd6c0ec8ee0a4b7a
Vitalii <vitalikkalyta96@gmail.com>
2021-11-02 23:36:20 +0200

    Updates todo.py.

--- flask3/routes/api/todo.py
+++ flask3/routes/api/todo.py
@@ -5,6 +5,7 @@ from flask_restful import Resource
 
 todos = []
 
+
 class Todo(Resource):
     def get(self):
         return todos
@@ -12,15 +13,38 @@ class Todo(Resource):
     def post(self):
         todos.append(request.json)
         return todos
-        
-        
-# def get_by_data(data):
-#    for x in todos:
-#        if x.get("title") == int(data):
-#            return x
 
 
 class Todo2(Resource):
+
+    def get(self, title):
+        data = todos[title]
+        for el in data:
+            if el['title'] == title:
+                return el
+        return Response("Data not found", 404)
+
+    def put(self, title, data):
+        todos[title] = request.json.get('text')
+        for x in range(len(todos[title])):
+            if todos[title][x]['title'] == title:
+                todos[title][x] = data
+                break
+            return todos[title]
+
+    def delete(self, title):
+        todos[title] = request.json.get('text')
+        for x in range(len(todos[title])):
+            if todos[title][x]['title'] == title:
+                del todos[title]
+                break
+            return Response("", 204)
+
+
+api.add_resource(Todo, "/api/v1/todos")
+api.add_resource(Todo2, "/api/v1/todos/<int:title>", methods=['PUT', 'DELETE'])
+
+
 
 #    def get(self, title):
 #       user = get_by_data(title)
@@ -28,27 +52,50 @@ class Todo2(Resource):
 #            return {"error": "User not found"}
 #        return user
 
-    def get(self, title):
-        data = todos[title]
-        if not data in todos:
-            return Response("Data not found", 404)
-        return todos[title]
-        
+# @classmethod
+# def get_by_id(cls, id):
+#     data = cls.get_file_data(cls.file)
+#     for el in data:
+#         if el['id'] == id:
+#             return el
+#
+#     raise Exception("Not found")
+
+# def get(self, id):
+#     return Salon.get_by_id(id)
+
+
 #    def put(self, title):
 #        user = get_by_data(title)
 #        if user:
 #            todos.remove(user)
 #            todos.append(request.json)
-#        return todos  
+#        return todos
 
-    def put(self, title):  
-        todos[title] =  request.json.get('text')
-        return todos[title]
+# def update_by_id(cls, id, data):
+#     items = cls.get_file_data(cls.file)
+#     for i in range(len(items)):
+#         if items[i]['id'] == id:
+#             items[i] = data
+#             break
+#     cls.save_to_file(items)
 
-    def delete(self, title):
-        del todos[title]
-        return Response("", 204)
-        
-api.add_resource(Todo, "/api/v1/todos")
-api.add_resource(Todo2, "/api/v1/todos/<int:title>", methods=['PUT', 'DELETE'])
+# def put(self, id):
+#     data = request.json
+#     Salon.update_by_id(id, data)
+#     return Salon.get_by_id(id)
+
+
+# @classmethod
+# def delete_by_id(cls, id):
+#     items = cls.get_file_data(cls.file)
+#     for i in range(len(items)):
+#         if items[i]['id'] == id:
+#             del items[i]
+#             break
+#     cls.save_to_file(items)
+#
+# def delete(self, id):
+#     Salon.delete_by_id(id)
+#     return "", 204
 
