0f6df7b8e7a8c598f832075a85b432dc3183b766
Vitalii <vitalikkalyta96@gmail.com>
2021-11-02 20:18:12 +0200

    Updated todo.py .

--- flask3/routes/api/todo.py
+++ flask3/routes/api/todo.py
@@ -1,9 +1,9 @@
 from app import app, api
-from flask import request
+from flask import request, Response
 from flask_restful import Resource
 
-todos = []
 
+todos = []
 
 class Todo(Resource):
     def get(self):
@@ -14,39 +14,40 @@ class Todo(Resource):
         return todos
         
         
-# Створив функцію для діставання title для редагування і видалення даних.
-# Чи варто її створювати чи є якийсь інший шлях для доступу до даних ???
+# def get_by_data(data):
+#    for x in todos:
+#        if x.get("title") == int(data):
+#            return x
 
-def get_user_by_data(user_data):
-    for x in todos:
-        if x.get("title") == str(user_data):
-            return x
-
-# Створив окермий клас для вирішення 1 завадння 15 дз. Чи я на вірному шляху чи я щось упустив, бо наразі не раниться коректоно?
-# Гуглив однак там в методи класу додають переважно ід чи тут потрібно також ід чи потрібно через title??
-#
 
 class Todo2(Resource):
 
+#    def get(self, title):
+#       user = get_by_data(title)
+#        if not user:
+#            return {"error": "User not found"}
+#        return user
+
     def get(self, title):
-        user = get_user_by_data(title)
-        if not user:
-            return {"error": "User not found"}
-        return user
+        data = todos[title]
+        if not data in todos:
+            return Response("Data not found", 404)
+        return todos[title]
         
-    def put(self, title):
-        user = get_user_by_data(title)
-        if user:
-            todos.remove(user)
-            todos.append(request.json)
-        return todos        
+#    def put(self, title):
+#        user = get_by_data(title)
+#        if user:
+#            todos.remove(user)
+#            todos.append(request.json)
+#        return todos  
+
+    def put(self, title):  
+        todos[title] =  request.json.get('text')
+        return todos[title]
 
     def delete(self, title):
-        return delete_todo()
-        user = get_user_by_data(title)
-        if user:
-            todos.remove(user)
-        return {"message": "Deleted"}
+        del todos[title]
+        return Response("", 204)
         
 api.add_resource(Todo, "/api/v1/todos")
-api.add_resource(Todo2, "/api/v1/todos/<string:title>", methods=['PUT', 'DELETE'])
+api.add_resource(Todo2, "/api/v1/todos/<int:title>", methods=['PUT', 'DELETE'])
--- flask3/routes/todo.py
+++ flask3/routes/todo.py
@@ -9,20 +9,3 @@ def todos_list():
 @app.route('/add-todo')
 def add_todo():
     return render_template('add-todo.html')
-    
-    
-# Чи варто зачіпати цю todo.py цей блок для вирішення 1 завдання з додаванням методу апдейт і деліт ??
-
-# @app.route('/update-todo/<string:title>/<string:text>/', methods=['PUT'])
-# def update_todo(title, text):
-#     return jsonify({title=title, text=text})
-
-# @app.route('/delete-todo/<string:title>/<string:text>/', methods=['DELETE'])
-# def delete_todo(title, text):
-#    todos.remove(title[0], text[1])
-#    return None, 404
-
-
-# @app.route('/delete-todo')
-# def add_todo():
-#    return render_template('delete-todo.html')
