229c203efb3747d9e70d6e8c81c36321a4f43895
Vitalii <vitalikkalyta96@gmail.com>
2021-11-16 01:08:20 +0200

    Done HW 20.

--- flask7/routes/main.py
+++ flask7/routes/main.py
@@ -5,25 +5,26 @@ from models.models import Plant, Employee, Salon
 
 @app.route('/')
 def main():
-     plants = Plant.query.all()
-     employees = Employee.query.all()
-     salons = Salon.query.all()
-     return render_template('index.html', plants=plants, employees=employees, salons=salons)
+    plants = Plant.query.all()
+    employees = Employee.query.all()
+    salons = Salon.query.all()
+    return render_template('index.html', plants=plants, employees=employees, salons=salons)
 
 
 @app.route('/plant/<int:id>')
 def plant(id):
-     plant = Plant.query.get(id)
-     return render_template('plant.html', plant=plant)
+    plant = Plant.query.get(id)
+    return render_template('plant.html', plant=plant)
 
 
 @app.route('/employee/<int:id>')
 def employee(id):
-     employee = Employee.query.get(id)
-     employee.department = Plant.query.get(employee.department_id)
-     return render_template('employee.html', employee=employee)
+    employee = Employee.query.get(id)
+    employee.department = Plant.query.get(employee.department_id)
+    return render_template('employee.html', employee=employee)
      
+ 
 @app.route('/salon/<int:id>')
 def salon(id):
-     salon = Salon.query.get(id)
-     return render_template('salon.html', salon=salon)
+    salon = Salon.query.get(id)
+    return render_template('salon.html', salon=salon)
