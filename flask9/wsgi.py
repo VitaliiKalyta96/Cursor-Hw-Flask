6d6a58f5f80f59746c5708414b248a93e88773ef
Vitalii <vitaliikalyta96@gmail.com>
2021-11-24 15:10:41 +0200

    Flask 9.

---
+++ flask9/wsgi.py
@@ -0,0 +1,11 @@
+# import sys
+# import logging
+# from app import app as application
+#
+# logging.basicConfig(stream=sys.stderr)
+# sys.path.insert(0,"/root/customer-account-automation/")
+#
+# app = application
+#
+# if __name__ == "__main__":
+#     application.run()
