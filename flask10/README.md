6906107dfa6a2b7d221bed92900e3f38d5318d08
Vitalii <vitaliikalyta96@gmail.com>
2021-11-27 22:48:58 +0200

    Flask 10 .

--- flask10/README.md
+++ flask10/README.md
@@ -3,4 +3,7 @@ Create file .env. Then write in this file follow information:
 Value1 - PYTHONUNBUFFERED=1
 
 Value2 SQLALCHEMY_DATABASE_URI=mysql+pymysql://password:login@host:port/database_name
+
+Value3 SECRET_KEY=____
+
 .
--- flask10/app.py
+++ flask10/app.py
@@ -18,7 +18,8 @@ db.init_app(app)
 with app.app_context():
     from routes.api import *
     from routes.main import *
-    from models import Plant, Employee, Salon
+    from models.models import Plant, Employee, Salon
+
 
 if __name__ == '__main__':
-    app.run(debug=True, host="0.0.0.0", port=8080)
+    app.run(debug=True, host="0.0.0.0", port=9090)
