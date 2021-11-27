8f5e0e0da86b114724bb06a756c08ac499bf6394
Vitalii <vitaliikalyta96@gmail.com>
2021-11-27 19:17:01 +0200

    Flask 10 .

---
+++ flask10/migrations/versions/cf13184dafd1_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: cf13184dafd1
+Revises: 
+Create Date: 2021-11-17 23:11:43.464957
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'cf13184dafd1'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
