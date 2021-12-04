db0f5faeeded8041efc6ed430d0532397cd06032
Vitalii Kalyta <86627796+VitaliiKalyta96@users.noreply.github.com>
2021-12-04 21:52:50 +0200

    Done HW 18. Added tests(update) .

--- flask6/tests/conftest.py
+++ flask6/tests/conftest.py
@@ -1,5 +1,5 @@
 import pytest
-from models.models import Plant, Employee, MenuItem, Salon
+from models import Plant, Employee, MenuItem, Salon
 from app import app, db
 
 
@@ -13,13 +13,23 @@ def client():
         db.session.commit()
     yield client
 
+
 @pytest.fixture()
 def plant_data():
     yield {
         'location': 'Ternopil',
         'name': 'Jacktom'
     }
-    
+
+
+@pytest.fixture()
+def plant_update():
+    yield {
+        'location': 'Chernivtsi',
+        'name': 'Jaison'
+    }
+
+
 @pytest.fixture()
 def employee_data():
     yield {
@@ -27,13 +37,32 @@ def employee_data():
         'name': 'Jack',
         'department_type': 'technic'
     } 
-        
+
+
+@pytest.fixture()
+def employee_update():
+    yield {
+        'email': 'alex@gmail.com',
+        'name': 'Alex',
+        'department_type': 'technic2'
+    }
+
+
 @pytest.fixture()
 def menuitem_data():
     yield {
         'name': 'Plant_Employee_Salon',
         'link': 'menu'
     }
+
+
+@pytest.fixture()
+def menuitem_update():
+    yield {
+        'name': 'Plant_Employee_Salon_2',
+        'link': 'menu_2'
+    }
+
 
 @pytest.fixture()
 def salon_data():
@@ -42,3 +71,13 @@ def salon_data():
         'city': 'Lviv',
         'address': 'Washington street 20'
     }
+
+
+@pytest.fixture()
+def salon_update():
+    yield {
+        'name': 'Jack',
+        'city': 'Ternopil',
+        'address': 'Avenue street 25'
+    }
+
